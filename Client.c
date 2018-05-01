4d245a440f84fe237b995a547e3c78b23f5ca910
Roguebantha <starwarsfreak000@gmail.com>
2018-05-01 15:55:58 -0400

    Major stabilization improvements and memory leak fixes. Just need to parse replies, and it'll be raring for primetime

--- Client.c
+++ Client.c
@@ -6,7 +6,7 @@ const char filename[128] = "all_passwords.dict";
 const char hash_filename[128] = "hashfile.hash";
 const unsigned int hash_type = 400;
 unsigned int benchmark() {
-	system("cd ./hashcat-4.1.0/ && cat example.dict | ./hashcat64.bin -m400  example400.hash --speed-only --machine-readable --quiet -O | grep 1: | cut -d : -f2 > ../benchmark_score");
+	system("cd ./hashcat-4.1.0/ && cat example.dict | ./hashcat64.bin -m400  example400.hash --speed-only --machine-readable --quiet -O --potfile-disable | grep 1: | cut -d : -f2 > ../benchmark_score");
 	unsigned int score;
 	FILE *benchmark = fopen("benchmark_score", "rb");
 	fscanf(benchmark,"%u",&score);
@@ -15,16 +15,17 @@ unsigned int benchmark() {
 }
 
 void crack(char* hash, unsigned int start, unsigned int end) {
+	printf("Cracking...\n");
 	char command[256];
+	sprintf(command,"rm -f %s",hash_filename);
+	system(command);
 	FILE *hashfile =  fopen(hash_filename,"ab+");
 	fputs(hash,hashfile);
 	fputs("\n",hashfile);
 	fclose(hashfile);
 	sprintf(command,"tail -n +%u %s | head -n $((%u-%u+1)) > passwords.dict",start,filename,end,start);
-	printf("%s\n",command);
 	system(command);
-	sprintf(command,"cat passwords.dict | ./hashcat-4.1.0/hashcat64.bin -m %d --machine-readable --quiet -O %s > output 2> output",hash_type,hash_filename);
-	printf("%s\n",command);
+	sprintf(command,"cat passwords.dict | ./hashcat-4.1.0/hashcat64.bin -m %d --machine-readable --quiet -O --potfile-disable %s | grep : > output 2> output",hash_type,hash_filename);
 	system(command);
 }
 int main() {
@@ -64,15 +65,22 @@ int main() {
 
 		FILE *output = fopen("output", "rb");
 		fseek(output, 0, SEEK_END);
-		long output_size = ftell(output);
+		unsigned int output_size = ftell(output);
 		fseek(output, 0, SEEK_SET);
-		char *result = malloc(output_size + 1);
+		char *result;
 		char *result_reply = malloc(output_size+30);
-		fread(result, output_size, 1, output);
+		if(output_size) {
+			result = malloc(output_size + 1);
+			fread(result, output_size, 1, output);
+			result[output_size] = 0;
+		} else {
+			result = malloc(16);
+			strcpy(result,"FAILED");
+		}
 		fclose(output);
-		result[output_size] = 0;
-
 		sprintf(result_reply,"1 %u %s",(unsigned int)(time(NULL) - t0),result);
 		send_bytes(result_reply,strlen(result_reply));
+		free(result_reply);
+		free(result);
 	}
 }
