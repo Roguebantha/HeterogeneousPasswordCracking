92b45fb648d775b07f0b89040990b68466f763ba
AstridKery <ckery1@umbc.edu>
2018-05-03 16:12:19 -0400

    hopefully this works now

--- load_balancer.c
+++ load_balancer.c
@@ -10,8 +10,17 @@
 #define LINES_IN_FILE 129988
 pthread_mutex_t lock;
 char hashed_password[1024];
+int total_benchmark = 0;
 //The function for each thread to run
 void *connection_handler(void*);
+
+int line_counter = 0;
+
+struct thread_send {
+  int sock;
+  int thread_id;
+  int benchmark;
+};
 
 int main()
 {
@@ -19,7 +28,13 @@ int main()
   struct sockaddr_in server, client;
   int connections = 0;
   pthread_t thread_id[10];
+  char answer[10];
+  int size, benchmark;
+  char incoming_message[4096];
+  int message_type;
+  char client_ip[20];
 
+  struct thread_send all_args[10];
   //Steps
   //Get the password from the user as an input
   printf("Please input the hashed password: ");
@@ -54,17 +69,44 @@ int main()
     return 1;
   }
 
+
   while(client_sock=accept(socket_desc,(struct sockaddr*)&client,(socklen_t*)&c))
   {
     printf("Accepted a connection\n");
-    if(pthread_create(&thread_id[connections], NULL, connection_handler, (void*) &client_sock) < 0)
-    {
-      printf("Could not create thread\n");
-    }
+
+
+    all_args[connections].sock = client_sock;
+    all_args[connections].thread_id =  connections;
+
+
+    memset(incoming_message,0,4096);
+    size = recv(client_sock,incoming_message, 4096, 0);
+    //previous_performance = benchmark
+
+    sscanf(incoming_message,"%d %s %d",&message_type,client_ip,&benchmark);
+    total_benchmark += benchmark;
+    all_args[connections].benchmark = benchmark;
+
     connections ++;
+    printf("Any more connections to make (Y/N)?\n");
+    fgets(answer,10,stdin);
+
+    if(strstr(answer,"N") != 0)
+    {
+      break;
+    }
+
   }
 
-  for(i = 0; i <= connections; i++)
+  for(i = 0; i < connections; i++)
+  {
+    if(pthread_create(&thread_id[i], NULL, connection_handler, (void*) &all_args[i]) < 0)
+    {
+      printf("Could not create threads\n");
+    }
+  }
+
+  for(i = 0; i < connections; i++)
   {
     pthread_join(thread_id[i],NULL);
   }
@@ -73,31 +115,27 @@ int main()
   return 0;
 }
 
-void *connection_handler(void*socket_desc)
+void *connection_handler(void*a)
 {
-  //This holds the current line they are on
-  int line_counter = 0;
-  int done = 0;
   int size, lines_to_send;
-  int sock = *(int*)socket_desc;
+  struct thread_send* args = (struct thread_send*)a;
+  int sock = args->sock;
   char incoming_message[4096];
   char send_buffer[2048];
   char result[1024];
   char client_ip[40];
-  int benchmark;
+  int benchmark = args->benchmark;//This needs to be a local variable
   int message_type;
+  char * password;
+
 
   //get the first message with the benchmarks from the client
-  size = recv(sock,incoming_message, 4096, 0);
-  //previous_performance = benchmark
 
-  sscanf(incoming_message,"%d %s %d",&message_type,client_ip,&benchmark);
-  printf("Now we have benchmark: %d and ip %s\n",benchmark,client_ip);
   //while the line counter is less than the number of lines or done == false
-  while(line_counter < LINES_IN_FILE && done == 0)
+  while(line_counter < LINES_IN_FILE)
   {
     //select the number of lines to send (based on previous performance)
-    lines_to_send = 200;//benchmark * 2000;
+    lines_to_send = (int)((float)benchmark/(float)total_benchmark) * 100000;//benchmark * 2000;
     //send the client the line counter and the number of lines
     sprintf(send_buffer,"%s %d %d",hashed_password,line_counter,line_counter + lines_to_send);
     if(send(sock,send_buffer,strlen(send_buffer),0) == -1)
@@ -112,16 +150,32 @@ void *connection_handler(void*socket_desc)
 
     //spin around waiting for a reply from the client saying they are done or
     //have found the item
+    printf("getting a reply\n");
+    memset(incoming_message,0,4096);
     size = recv(sock,incoming_message,4096,0);
+
     incoming_message[size] = "\0";
     sscanf(incoming_message,"%d %d %s",&message_type,&benchmark,result);
-    printf("Got %s",incoming_message);
 
+    if((password = strstr(result,":")) != 0)
+    {
+      printf("Password found %s\n",++password);
+      if(send(sock,"exit\0",5,0) == -1)
+      {
+        printf("Can't tell client to exit");
+        return 1;
+      }
+      return 0;
+    }
+
+  }
+
+  //Nothing was ever found
+  printf("No password found :( \n");
+  if(send(sock,"exit\0",5,0) == -1)
+  {
+    printf("Can't tell client to exit");
     return 1;
-
-
-    //Take the time they took to do it and compare with expected to get
-    //previous_performance
   }
  //If we ever found the answer, print it out
 
